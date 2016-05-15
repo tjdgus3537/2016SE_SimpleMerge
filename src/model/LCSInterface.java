e2549bb428a0b2e4b4ed6439d5db4320b460345b
tjdgus3537 <tjdgus3537@naver.com>
2016-05-15 16:41:26 +0900

    create LCSInterface

--- src/model/LCSInterface.java
+++ src/model/LCSInterface.java
@@ -1,5 +1,12 @@
 package model;
 
-public interface LCSInterface {
+/**
+ * Created by Seonghyeon on 5/15/2016.
+ * 두 string에 대해 LCS algorithm을 적용한 결과를 준다.
+ * input : two string
+ * output : string들을 이용해서 만든 lcs 결과값
+ */
 
+public interface LCSInterface {
+	public String getLCS(String firstString, String secondString);
 }
