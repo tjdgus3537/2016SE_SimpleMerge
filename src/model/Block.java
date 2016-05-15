b9f9c9c1ae44eab34d1bd3ff00e0bfb8963f75f2
tjdgus3537 <tjdgus3537@naver.com>
2016-05-15 21:10:43 +0900

    Block에서 state가 SPACE인 경우 startNumber는 의미를 갖지 못하기 때문에 항상 -1이 된다.

--- src/model/Block.java
+++ src/model/Block.java
@@ -4,6 +4,8 @@ package model;
  * Created by Seonghyeon on 5/15/2016.
  * state가 동일한 주변의 line들을 묶은 단위.
  * 시작하는 줄의 번호 (startNumber), 줄의 개수(numberOfLine), 상태 여부 (state)를 갖는다.
+ * SPACE 상태를 갖는 block은 시작 줄은 의미가 없고, 줄의 개수만 의미를 갖는다.
+ * 즉, SPACE 상태일 때는 항상 startNumber가 -1 이다.
  */
 
 public class Block implements StateUsable{
@@ -19,6 +21,8 @@ public class Block implements StateUsable{
 	
 	public void setStartNumber(int startNumber) {
 		this.startNumber = startNumber;
+		if(state == SPACE)
+			this.startNumber = -1;
 	}
 	
 	public void setNumberOfLine(int numberOfLine) {
@@ -27,6 +31,8 @@ public class Block implements StateUsable{
 	
 	public void setState(int state) {
 		this.state = state;
+		if(state == SPACE)
+			setStartNumber(-1);
 	}
 	
 	public int getStartNumber() {
