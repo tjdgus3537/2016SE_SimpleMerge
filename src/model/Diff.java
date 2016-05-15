63b83132c0348f7afe135775f7a131a9a0e3ce72
tjdgus3537 <tjdgus3537@naver.com>
2016-05-15 22:32:36 +0900

    create Diff Class

---
+++ src/model/Diff.java
@@ -0,0 +1,46 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Diff implements DiffInterface, StateUsable{
+
+	@Override
+	public PairBlockArrayList compare(String left, String right) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String blockCopyToLeft(String left, String right, int blockNumber,
+			PairBlockArrayList pairBlockArrayList) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String blockCopyToRight(String left, String right, int blockNumber,
+			PairBlockArrayList pairBlockArrayList) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String allCopyToLeft(String left, String right) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String allCopyToRight(String left, String right) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String ApplySpaceBlockToEnter(String s, ArrayList<Block> blocks) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+}
