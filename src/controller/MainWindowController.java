17d956d96565f55c34e293c305dfd4dffcc32213
Donghwan Kim <wraithtorch@gmail.com>
2016-05-15 22:37:47 +0900

    AnckorPane으로 각 컴포넌트를 모서리에 고정함

--- src/controller/MainWindowController.java
+++ src/controller/MainWindowController.java
@@ -2,22 +2,26 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * Created by Donghwan on 5/14/2016.
  * 메인 윈도우의 액션을 관리하는 컨트롤러
  */
-public class MainWindowController {
+public class MainWindowController implements Initializable{
     @FXML
-    private Button compareButton;
-    @FXML
-    private Button copyToRightButton;
-    @FXML
-    private Button copyToLeftButton;
+    private SplitPane editorSplitPane;
 
     @FXML
-    protected void handelCompareButtonAction(ActionEvent event){
+    protected void handleCompareButtonAction(ActionEvent event){
         System.out.println("comp");
     }
 
@@ -29,5 +33,18 @@ public class MainWindowController {
     @FXML
     protected void handelCopyToLeftButtonAction(ActionEvent event){
         System.out.println("copytoleft");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources){
+        try{
+            AnchorPane leftEditorPane = FXMLLoader.load(getClass().getResource("/fxml/EditorPane.fxml"));
+            AnchorPane rightEditorPane = FXMLLoader.load(getClass().getResource("/fxml/EditorPane.fxml"));
+            editorSplitPane.getItems().add(leftEditorPane);
+            editorSplitPane.getItems().add(rightEditorPane);
+            editorSplitPane.setDividerPositions(0.5);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
