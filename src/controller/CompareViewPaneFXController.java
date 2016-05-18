1eb9d25d43317928ab5ab6070cf0d47eaec9e092
Donghwan Kim <wraithtorch@gmail.com>
2016-05-18 13:16:08 +0900

    create CompareViewPane with  controller

---
+++ src/controller/CompareViewPaneFXController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Donghwan on 5/18/2016.
+ * 비교 결과 창을 관리하는 컨트롤러
+ */
+public class CompareViewPaneFXController implements Initializable{
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO compare 결과물 생성 및 테이블 뷰에 넣기
+    }
+}
