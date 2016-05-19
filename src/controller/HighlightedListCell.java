a35a02ab06ad2daa887600dc63ed63164814e699
Donghwan Kim <wraithtorch@gmail.com>
2016-05-20 04:31:19 +0900

    add style to horizontal scrollbar in textarea and create highlightedListCell

---
+++ src/controller/HighlightedListCell.java
@@ -0,0 +1,16 @@
+package controller;
+
+import javafx.scene.control.ListCell;
+
+/**
+ * Created by Donghwan on 5/20/2016.
+ * 리스트 뷰에서 비교 결과를 하이라이팅하는 셀
+ */
+public class HighlightedListCell extends ListCell<String> {
+    @Override
+    protected void updateItem(String item, boolean empty) {
+        super.updateItem(item, empty);
+        // TODO 블럭 결과에 따라서 달라야 함
+
+    }
+}
