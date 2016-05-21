1118a2e670176a73d9f041bd77c936ebf3390f59
tjdgus3537 <tjdgus3537@naver.com>
2016-05-22 03:58:05 +0900

    create DiffCommand Class

---
+++ src/model/DiffCommand.java
@@ -0,0 +1,11 @@
+package model;
+
+/**
+ * Created by Seonghyeon on 5/22/2016.
+ * Diff를 수행하는 곳과 Diff 호출하는 곳을 분리해주는 Class
+ * Controller에게 호출 당하면 Model에서 data를 가져와서 기능을 수행하고 결과를 Model에 반영한다.
+ */
+
+public class DiffCommand {
+
+}
