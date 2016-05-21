2cef965e6f05e4fc7f1a23cc981542084b7cd7d1
Donghwan Kim <wraithtorch@gmail.com>
2016-05-22 03:18:33 +0900

    create HighlightedListCell

--- src/controller/HighlightedListCell.java
+++ src/controller/HighlightedListCell.java
@@ -1,16 +1,26 @@
 package controller;
 
+import javafx.collections.ObservableList;
 import javafx.scene.control.ListCell;
+import model.Block;
 
 /**
  * Created by Donghwan on 5/20/2016.
  * 리스트 뷰에서 비교 결과를 하이라이팅하는 셀
  */
-public class HighlightedListCell extends ListCell<String> {
+public class HighlightedListCell extends ListCell<Block> {
     @Override
-    protected void updateItem(String item, boolean empty) {
+    protected void updateItem(Block item, boolean empty) {
         super.updateItem(item, empty);
-        // TODO 블럭 결과에 따라서 달라야 함
-
+        setText(item.getContent());
+        ObservableList<String> styleList = getStyleClass();
+        styleList.removeAll("changed", "unchanged", "space");
+        if(item.isChanged()) {
+            styleList.add("changed");
+        }else if(item.isSpace()){
+            styleList.add("space");
+        }else if(item.isUnchanged()){
+            styleList.add("unchanged");
+        }
     }
 }
