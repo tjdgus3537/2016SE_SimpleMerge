cd80e427d11354901cf59c3e324c9fd924724cc5
tjdgus3537 <tjdgus3537@naver.com>
2016-05-23 02:35:45 +0900

    CompareModel Class 생성

--- src/model/CompareModel.java
+++ src/model/CompareModel.java
@@ -2,11 +2,43 @@ package model;
 
 import java.util.ArrayList;
 
+import sun.security.jca.GetInstance;
+
 /**
  * Created by Seonghyeon on 5/22/2016.
  * compare mode에서 사용되는 data와 관련된 model
  */
 
-public class CompareModel {
+public class CompareModel implements CompareModelInterface{
+	private static CompareModel compareModel = new CompareModel();
+	private ArrayList<BlockReadInterface> left;
+	private ArrayList<BlockReadInterface> right;
+	
+	private CompareModel() {
+	}
 
+	public static CompareModel getInstance() {
+		return compareModel;
+	}
+	
+	public void setLeft(ArrayList<BlockReadInterface> blocks) {
+		left = blocks;
+	}
+
+	@Override
+	public void setRight(ArrayList<BlockReadInterface> blocks) {
+		right = blocks;
+	}
+
+	@Override
+	public ArrayList<BlockReadInterface> getLeft() {
+		return left;
+	}
+
+	@Override
+	public ArrayList<BlockReadInterface> getRight() {
+		return right;
+	}
+	
+	
 }
