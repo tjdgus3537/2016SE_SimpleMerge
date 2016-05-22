40d2aeca6b74ffc3e91028e5e638fa59132c072f
tjdgus3537 <tjdgus3537@naver.com>
2016-05-23 02:44:46 +0900

    complement PariBlocksReadOnlyInterface in PairBlocks Class

--- src/model/PairBlocks.java
+++ src/model/PairBlocks.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
  * Diff Class에서 leftString과 rightString이 input으로 주어지고 Compare 했을 때 return되는 data의 type
  */
 
-public class PairBlocks {
+public class PairBlocks implements PairBlocksReadOnlyInterface{
 	private ArrayList<Block> left;
 	private ArrayList<Block> right;
 	
