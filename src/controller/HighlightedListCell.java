79751da1be2a8c44b886de7527237fcf40d1f5c2
Donghwan Kim <wraithtorch@gmail.com>
2016-05-22 13:09:26 +0900

    add generic for highlightedListCell - 리스트셀이 BlockReadInterface를 상속받는 데이터를 받아들여야 하므로 그에 대한 제너릭을 추가함

--- src/controller/HighlightedListCell.java
+++ src/controller/HighlightedListCell.java
@@ -3,14 +3,15 @@ package controller;
 import javafx.collections.ObservableList;
 import javafx.scene.control.ListCell;
 import model.Block;
+import model.BlockReadInterface;
 
 /**
  * Created by Donghwan on 5/20/2016.
  * 리스트 뷰에서 비교 결과를 하이라이팅하는 셀
  */
-public class HighlightedListCell extends ListCell<Block> {
+public class HighlightedListCell<T extends BlockReadInterface> extends ListCell<T> {
     @Override
-    protected void updateItem(Block item, boolean empty) {
+    protected void updateItem(T item, boolean empty) {
         super.updateItem(item, empty);
         setText(item.getContent());
         ObservableList<String> styleList = getStyleClass();
