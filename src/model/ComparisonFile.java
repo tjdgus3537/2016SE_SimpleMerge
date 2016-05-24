f12fd57cf419be21f3322d7559c6d4e7e50527d6
Donghwan Kim <wraithtorch@gmail.com>
2016-05-25 00:32:06 +0900

    change comparisonFile to store only stringProperty

--- src/model/ComparisonFile.java
+++ src/model/ComparisonFile.java
@@ -1,5 +1,6 @@
 package model;
 
+import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -11,16 +12,12 @@ import java.util.stream.Collector;
  * Created by Donghwan on 5/12/2016.
  * 비교하는 파일에 대해서 저장하는 클래스
  */
-public class ComparisonFile implements TextPropertyProvider, ObservableBlocksProvider{
+public class ComparisonFile implements TextPropertyProvider{
     private File source;
-    private ObservableList<BlockReadInterface> content;
+    private StringProperty content;
 
     public ComparisonFile(File source, String content){
-        setSource(source);
-        setContent(content);
-    }
-
-    public ComparisonFile(File source, ObservableList<BlockReadInterface> content) {
+        this.content = new SimpleStringProperty();
         setSource(source);
         setContent(content);
     }
@@ -29,35 +26,19 @@ public class ComparisonFile implements TextPropertyProvider, ObservableBlocksPro
         this.source = source;
     }
 
-    public void setContent(ObservableList<BlockReadInterface> content) {
-        this.content = content;
-    }
-
     public void setContent(String content){
-        if(this.content != null) this.content.clear();
-        else this.content = FXCollections.observableArrayList();
-        Block block = new Block(content);
-        this.content.add(block);
+        this.content.setValue(content);
     }
 
     public File getSource() {
         return source;
     }
-
     @Override
-    public ObservableList<BlockReadInterface> getContent() {
+    public StringProperty textProperty(){
         return content;
     }
 
-    @Override
-    public StringProperty textProperty(){
-        Block singleBlock = new Block(content.stream().filter(block->block.getState() != State.SPACE).collect(Collector.of(()->new StringBuffer(), (buf, item)-> buf.append(item.getContent()), (buf1, buf2) -> buf1.append(buf2), StringBuffer::toString)));
-        content.clear();
-        content.add(singleBlock);
-        return singleBlock.contentProperty();
-    }
-
     public String getContentToString() {
-        return content.stream().filter(block->block.getState() != State.SPACE).collect(Collector.of(()->new StringBuffer(), (buf, item)-> buf.append(item.getContent()), (buf1, buf2) -> buf1.append(buf2), StringBuffer::toString));
+        return content.getValue();
     }
 }
