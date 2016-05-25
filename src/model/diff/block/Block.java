8d95c59a71b1774aa3d0f6d00a4880da4f5f19be
tjdgus3537 <tjdgus3537@naver.com>
2016-05-26 01:17:44 +0900

    Diff Class 변화, setter 불가능, BlockReadOnlyInterface를 쓸 필요 없이 Block을 View에서 바로 사용가능

--- src/model/diff/block/Block.java
+++ src/model/diff/block/Block.java
@@ -3,62 +3,39 @@ package model.diff.block;
 /**
  * Created by Seonghyeon on 5/15/2016.
  * state가 동일한 주변의 line들을 묶은 단위.
- * 시작하는 줄의 번호 (startNumber), 줄의 개수(numberOfLine), 상태 여부 (state),
- * 내용(content)를 갖는다.
+ * View에서 출력할 목적으로 사용하기 때문에 setter는 제공하지 않고 getter만 제공한다.
+ * Block을 생성할 때만 생성자를 통해 값을 넣고, 그 이후에 Block을 수정할 수 없다.
+ * 상태 여부 (state)와 내용(content)를 갖는다.
  */
 
 public class Block{
-	private int startNumber;
-	private int numberOfLine;
 	private State state;
 	private String content;
 	
 	public Block() {
-		setState(State.SPACE);
-		setStartNumber(-1);
-		setNumberOfLine(0);
+		setState(State.UNCHANGED);
 		setContent(null);
 	}
 
-	public Block(int startNumber, int numberOfLine, State state, String content) {
+	public Block(State state, String content) {
 		this();
 		setState(state);
-		setStartNumber(startNumber);
-		setNumberOfLine(numberOfLine);
 		setContent(content);
 	}
 
-	public void setContent(String content) {
-		this.content = content;
-	}
-	
-	public void setStartNumber(int startNumber) {
-		this.startNumber = startNumber;
-	}
-	
-	public void setNumberOfLine(int numberOfLine) {
-		this.numberOfLine = numberOfLine;
-	}
-	
-	public void setState(State state) {
-		this.state = state;
-	}
-	
-	public int getStartNumber() {
-		return startNumber;
-	}
-	
-	public int getNumberOfLine() {
-		return numberOfLine;
-	}
-
-	@Override
 	public State getState() {
 		return state;
 	}
 
-	@Override
 	public String getContent() {
 		return content;
+	}
+	
+	private void setContent(String content) {
+		this.content = content;
+	}
+	
+	private void setState(State state) {
+		this.state = state;
 	}
 }
