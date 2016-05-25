48a9f057e58bcb033d184cf8d9b8926af8ecb427
tjdgus3537 <tjdgus3537@naver.com>
2016-05-26 02:12:05 +0900

    CopierInterface 내부 결정 및 주석 넣음

--- src/model/diff/CopierInterface.java
+++ src/model/diff/CopierInterface.java
@@ -1,11 +1,23 @@
 package model.diff;
 
+import java.util.List;
+
+import model.diff.block.Block;
+
 /**
  * Created by Seonghyeon on 5/26/2016.
- * 2개의 ArrayList<Block>을 묶어둔 단위
- * Diff Class에서 leftString과 rightString이 input으로 주어지고 Compare 했을 때 return되는 data의 type
+ * copyToLeft, copyToRight를 제공해주는 interface
  */
 
 public interface CopierInterface {
-
+    /**
+     * blockNum에 대응하는 오른쪽 block을 왼쪽으로 copy하고, 필요하면 block을 통합함.
+     * @param compare의 결과 중 left
+     */
+	public void copyToLeft(List<Block> left, List<Block> right, int blockNum);
+	/**
+     * blockNum에 대응하는 왼쪽 block을 오른쪽으로 copy하고, 필요하면 block을 통합함.
+     * @param compare의 결과 중 right
+     */
+	public void copyToRight(List<Block> left, List<Block> right, int blockNum);
 }
