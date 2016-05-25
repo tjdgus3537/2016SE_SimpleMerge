56a009eadb2f28f99878f0c5f96487a16fef2c5a
tjdgus3537 <tjdgus3537@naver.com>
2016-05-26 03:11:37 +0900

    EditorModel Class 생성자 추가 및, 멤버 변수 추가

--- src/model/editorModel/EditorModel.java
+++ src/model/editorModel/EditorModel.java
@@ -1,7 +1,49 @@
 package model.editorModel;
 
+import java.io.File;
+
+import javafx.beans.property.ReadOnlyObjectProperty;
+import model.editorModel.contentNodeModel.ObservableListModelInterface;
+import model.editorModel.contentNodeModel.ObservableTextModelInterface;
+import model.fileIO.file.ObservableComparisonFile;
+
 /**
  * Created by Donghwan on 5/25/2016.
  */
 public class EditorModel implements EditorModelInterface {
+	ObservableComparisonFile comparisonFile;
+	
+	public EditorModel(ObservableComparisonFile comparisonFile) {
+		this.comparisonFile = comparisonFile;
+	}
+
+	@Override
+	public ReadOnlyObjectProperty<File> fileReadOnlyProperty() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void load(File source) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void save() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ObservableTextModelInterface getObservableTextModel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ObservableListModelInterface getObservableListModel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
