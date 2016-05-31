2c3f001e4048676015628ce9a2cf8782e22b153e
tjdgus3537 <tjdgus3537@naver.com>
2016-05-31 20:46:54 +0900

    DiffTest Class refactoring

--- src/test/java/DiffTest.java
+++ src/test/java/DiffTest.java
@@ -26,16 +26,16 @@ public class DiffTest {
 	}
 	
 	//helper method
-	private String pairBlockArrayListToString(PairBlocks pairBlockArrayList) {
+	private String pairBlocksToString(PairBlocks blocks) {
 		String s = "";
 		
-		for(int i = 0 ; i < pairBlockArrayList.getLeft().size(); i++)
-			s += blockToString(pairBlockArrayList.getLeft().get(i));
+		for(int i = 0 ; i < blocks.getLeft().size(); i++)
+			s += blockToString(blocks.getLeft().get(i));
 		
 		s += " ";
 		
-		for(int i = 0 ; i < pairBlockArrayList.getRight().size(); i++)
-			s += blockToString(pairBlockArrayList.getRight().get(i));
+		for(int i = 0 ; i < blocks.getRight().size(); i++)
+			s += blockToString(blocks.getRight().get(i));
 		
 		return s;
 	}
@@ -48,9 +48,9 @@ public class DiffTest {
 	//정상적인 형태의 임의의 case
 	@Test
 	public void testCompare1() {
-		PairBlocks pairBlockArrayList;
-		pairBlockArrayList = diff.compare("a\nb\nc\n", "\n\na\nb\n");
-		String s = pairBlockArrayListToString(pairBlockArrayList);
+		PairBlocks blocks;
+		blocks = diff.compare("a\nb\nc\n", "\n\na\nb\n");
+		String s = pairBlocksToString(blocks);
 		
 		assertEquals("\n\na\nb\nc\n \n\na\nb\n\n", s);
 	}
@@ -58,9 +58,9 @@ public class DiffTest {
 	//String 중 왼쪽이 비어있을 때
 	@Test
 	public void testCompare2() {
-		PairBlocks pairBlockArrayList;
-		pairBlockArrayList = diff.compare("", "\n\na\nb\n");
-		String s = pairBlockArrayListToString(pairBlockArrayList);
+		PairBlocks blocks;
+		blocks = diff.compare("", "\n\na\nb\n");
+		String s = pairBlocksToString(blocks);
 
 		assertEquals("\n\n\n\n \n\na\nb\n", s);
 	}
@@ -68,9 +68,9 @@ public class DiffTest {
 	//String 중 오른쪽이 비어있을 때
 	@Test
 	public void testCompare3() {
-		PairBlocks pairBlockArrayList;
-		pairBlockArrayList = diff.compare("\n\na\nb\n", "");
-		String s = pairBlockArrayListToString(pairBlockArrayList);
+		PairBlocks blocks;
+		blocks = diff.compare("\n\na\nb\n", "");
+		String s = pairBlocksToString(blocks);
 
 		assertEquals("\n\na\nb\n \n\n\n\n", s);
 	}
@@ -78,9 +78,9 @@ public class DiffTest {
 	//String 두 개가 모두 공스트링인 경우
 	@Test
 	public void testCompare4() {
-		PairBlocks pairBlockArrayList;
-		pairBlockArrayList = diff.compare("", "");
-		String s = pairBlockArrayListToString(pairBlockArrayList);
+		PairBlocks blocks;
+		blocks = diff.compare("", "");
+		String s = pairBlocksToString(blocks);
 
 		assertEquals(" ", s);
 	}
@@ -88,27 +88,27 @@ public class DiffTest {
 	//String 중 한 개가 null인 경우
 	@Test
 	public void testCompare5() {
-		PairBlocks pairBlockArrayList;
-		pairBlockArrayList = diff.compare(null, "");
+		PairBlocks blocks;
+		blocks = diff.compare(null, "");
 
-		assertEquals(null, pairBlockArrayList);
+		assertEquals(null, blocks);
 	}
 	
 	//String 두 개 모두 null인 경우
 	@Test
 	public void testCompare6() {
-		PairBlocks pairBlockArrayList;
-		pairBlockArrayList = diff.compare(null, null);
+		PairBlocks blocks;
+		blocks = diff.compare(null, null);
 
-		assertEquals(null, pairBlockArrayList);
+		assertEquals(null, blocks);
 	}
 	
 	//정상적인 형태의 임의의 case
 	@Test
 	public void testCompare7() {
-		PairBlocks pairBlockArrayList;
-		pairBlockArrayList = diff.compare("d\naabc\n", "aabc\nx\ny\n");
-		String s = pairBlockArrayListToString(pairBlockArrayList);
+		PairBlocks blocks;
+		blocks = diff.compare("d\naabc\n", "aabc\nx\ny\n");
+		String s = pairBlocksToString(blocks);
 		
 		assertEquals("d\naabc\n\n\n \naabc\nx\ny\n", s);
 	}
@@ -116,10 +116,10 @@ public class DiffTest {
 	//정상적인 형태의 임의의 case
 	@Test
 	public void testCompare8() {
-		PairBlocks pairBlockArrayList;
-		pairBlockArrayList = diff.compare("\n#ignore thumbnails created by windows\nThumbs.db\n",
+		PairBlocks blocks;
+		blocks = diff.compare("\n#ignore thumbnails created by windows\nThumbs.db\n",
 				"\nsyntax: glob\n\nThumbs.db\n");
-		String s = pairBlockArrayListToString(pairBlockArrayList);
+		String s = pairBlocksToString(blocks);
 		
 		assertEquals("\n#ignore thumbnails created by windows\n\n\nThumbs.db\n \n\nsyntax: glob\n\nThumbs.db\n", s);
 	}
@@ -127,10 +127,10 @@ public class DiffTest {
 	//정상적인 형태의 임의의 case
 	@Test
 	public void testCompare9() {
-		PairBlocks pairBlockArrayList;
-		pairBlockArrayList = diff.compare("\n#ignore thumbnails created by windows\nsyntax: glob\n\nThumbs.db\n#Ignore files build by Visual Studio\n*.obj\n*",
+		PairBlocks blocks;
+		blocks = diff.compare("\n#ignore thumbnails created by windows\nsyntax: glob\n\nThumbs.db\n#Ignore files build by Visual Studio\n*.obj\n*",
 				"syntax: glob\n\nThumbs.db\n*.obj\n*");
-		String s = pairBlockArrayListToString(pairBlockArrayList);
+		String s = pairBlocksToString(blocks);
 		
 		assertEquals("\n#ignore thumbnails created by windows\nsyntax: glob\n\nThumbs.db\n#Ignore files build by Visual Studio\n*.obj\n* \n\nsyntax: glob\n\nThumbs.db\n\n*.obj\n*", s);
 	}
