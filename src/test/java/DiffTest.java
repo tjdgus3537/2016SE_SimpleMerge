88f4b002f7285263c910e98228f2a046d2a55cf8
tjdgus3537 <tjdgus3537@naver.com>
2016-06-02 12:28:55 +0900

    DiffTest Class naming refactoring

--- src/test/java/DiffTest.java
+++ src/test/java/DiffTest.java
@@ -47,7 +47,7 @@ public class DiffTest {
 	
 	//정상적인 형태의 임의의 case
 	@Test
-	public void testCompare1() {
+	public void testCompareNormalCase1() {
 		PairBlocks blocks;
 		blocks = diff.compare("a\nb\nc\n", "\n\na\nb\n");
 		String s = pairBlocksToString(blocks);
@@ -55,57 +55,9 @@ public class DiffTest {
 		assertEquals("\n\na\nb\nc\n \n\na\nb\n\n", s);
 	}
 	
-	//String 중 왼쪽이 비어있을 때
-	@Test
-	public void testCompare2() {
-		PairBlocks blocks;
-		blocks = diff.compare("", "\n\na\nb\n");
-		String s = pairBlocksToString(blocks);
-
-		assertEquals("\n\n\n\n \n\na\nb\n", s);
-	}
-
-	//String 중 오른쪽이 비어있을 때
-	@Test
-	public void testCompare3() {
-		PairBlocks blocks;
-		blocks = diff.compare("\n\na\nb\n", "");
-		String s = pairBlocksToString(blocks);
-
-		assertEquals("\n\na\nb\n \n\n\n\n", s);
-	}
-	
-	//String 두 개가 모두 공스트링인 경우
-	@Test
-	public void testCompare4() {
-		PairBlocks blocks;
-		blocks = diff.compare("", "");
-		String s = pairBlocksToString(blocks);
-
-		assertEquals(" ", s);
-	}
-	
-	//String 중 한 개가 null인 경우
-	@Test
-	public void testCompare5() {
-		PairBlocks blocks;
-		blocks = diff.compare(null, "");
-
-		assertEquals(null, blocks);
-	}
-	
-	//String 두 개 모두 null인 경우
-	@Test
-	public void testCompare6() {
-		PairBlocks blocks;
-		blocks = diff.compare(null, null);
-
-		assertEquals(null, blocks);
-	}
-	
 	//정상적인 형태의 임의의 case
 	@Test
-	public void testCompare7() {
+	public void testCompareNormalCase2() {
 		PairBlocks blocks;
 		blocks = diff.compare("d\naabc\n", "aabc\nx\ny\n");
 		String s = pairBlocksToString(blocks);
@@ -115,7 +67,7 @@ public class DiffTest {
 	
 	//정상적인 형태의 임의의 case
 	@Test
-	public void testCompare8() {
+	public void testCompareNormalCase3() {
 		PairBlocks blocks;
 		blocks = diff.compare("\n#ignore thumbnails created by windows\nThumbs.db\n",
 				"\nsyntax: glob\n\nThumbs.db\n");
@@ -126,12 +78,60 @@ public class DiffTest {
 	
 	//정상적인 형태의 임의의 case
 	@Test
-	public void testCompare9() {
+	public void testCompareNormalCase4() {
 		PairBlocks blocks;
 		blocks = diff.compare("\n#ignore thumbnails created by windows\nsyntax: glob\n\nThumbs.db\n#Ignore files build by Visual Studio\n*.obj\n*",
 				"syntax: glob\n\nThumbs.db\n*.obj\n*");
 		String s = pairBlocksToString(blocks);
 		
 		assertEquals("\n#ignore thumbnails created by windows\nsyntax: glob\n\nThumbs.db\n#Ignore files build by Visual Studio\n*.obj\n* \n\nsyntax: glob\n\nThumbs.db\n\n*.obj\n*", s);
+	}
+	
+	//String 중 왼쪽이 비어있을 때
+	@Test
+	public void testCompareAbnormalCase1() {
+		PairBlocks blocks;
+		blocks = diff.compare("", "\n\na\nb\n");
+		String s = pairBlocksToString(blocks);
+
+		assertEquals("\n\n\n\n \n\na\nb\n", s);
+	}
+
+	//String 중 오른쪽이 비어있을 때
+	@Test
+	public void testCompareAbnormalCase2() {
+		PairBlocks blocks;
+		blocks = diff.compare("\n\na\nb\n", "");
+		String s = pairBlocksToString(blocks);
+
+		assertEquals("\n\na\nb\n \n\n\n\n", s);
+	}
+	
+	//String 두 개가 모두 공스트링인 경우
+	@Test
+	public void testCompareAbnormalCase3() {
+		PairBlocks blocks;
+		blocks = diff.compare("", "");
+		String s = pairBlocksToString(blocks);
+
+		assertEquals(" ", s);
+	}
+	
+	//String 중 한 개가 null인 경우
+	@Test
+	public void testCompareAbnormalCase4() {
+		PairBlocks blocks;
+		blocks = diff.compare(null, "");
+
+		assertEquals(null, blocks);
+	}
+	
+	//String 두 개 모두 null인 경우
+	@Test
+	public void testCompareAbnormalCase5() {
+		PairBlocks blocks;
+		blocks = diff.compare(null, null);
+
+		assertEquals(null, blocks);
 	}
 }
