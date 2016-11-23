3632e2c183b4cf7cef3b495bdf3cc9b0f5ad2054
SengHyun <SH@SengHyunui-MacBook-Pro.local>
2016-11-24 03:45:20 +0900

    testfx setting complete..

---
+++ src/test/java/GUITest.java
@@ -0,0 +1,14 @@
+import javafx.stage.Stage;
+import org.testfx.framework.junit.ApplicationTest;
+
+/**
+ * Created by SH on 2016. 11. 24..
+ */
+public class GUITest extends ApplicationTest {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+    }
+
+}
